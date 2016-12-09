77cad17245123761f2ea5084ea9fa84df6db0da1
Doug Latornell <doug.latornell@43ravens.ca>
2016-12-08 18:01:51 -0800

    Add tox testing of package under Python 3.5 and 2.7.

--- nemo_cmd/__pkg_metadata__.py
+++ nemo_cmd/__pkg_metadata__.py
@@ -1,1 +1,20 @@
-../__pkg_metadata__.py
+# Copyright 2013-2016 The Salish Sea MEOPAR Contributors
+# and The University of British Columbia
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""SalishSeaCmd -- Salish Sea NEMO command processor
+"""
+PROJECT = 'NEMO-Cmd'
+DESCRIPTION = 'NEMO Command Processor'
+VERSION = '1.0.dev0'
+DEV_STATUS = '3 - Alpha'
