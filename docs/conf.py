25148b7603783608cfc772007620c5295530a536
Doug Latornell <djl@douglatornell.ca>
2016-12-31 11:24:36 -0800

    Enable docs index generation.

--- docs/conf.py
+++ docs/conf.py
@@ -96,7 +96,7 @@ html_last_updated_fmt = '%b %d, %Y'
 html_domain_indices = True
 
 # If false, no index is generated.
-html_use_index = False
+html_use_index = True
 
 # If true, the index is split into individual pages for each letter.
 html_split_index = False
