0781196e4b61c3b771ffa8967ce06ff85fb9d6a5
Doug Latornell <djl@douglatornell.ca>
2017-03-20 16:14:45 -0700

    Change run sub-command description file command-line arg to Path object.

--- tests/test_run.py
+++ tests/test_run.py
@@ -56,7 +56,7 @@ class TestParser:
     def test_parsed_args_defaults(self, run_cmd):
         parser = run_cmd.get_parser('nemo run')
         parsed_args = parser.parse_args(['foo', 'baz'])
-        assert parsed_args.desc_file == 'foo'
+        assert parsed_args.desc_file == Path('foo')
         assert parsed_args.results_dir == 'baz'
         assert parsed_args.max_deflate_jobs == 4
         assert not parsed_args.nemo34
