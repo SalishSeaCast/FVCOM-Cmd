785bca839fedab59eb13d8ca431427619c2c36f8
Doug Latornell <djl@douglatornell.ca>
2017-04-04 14:06:01 -0700

    Fix bug re: use of expanded paths for coordinates and bathymetry files.
    re: issue # 17

--- nemo_cmd/prepare.py
+++ nemo_cmd/prepare.py
@@ -613,10 +613,8 @@ def _make_grid_links(run_desc, run_dir):
             run_desc, ('paths', 'forcing'), resolve_path=True, run_dir=run_dir
         )
         grid_dir = nemo_forcing_dir / 'grid'
-        grid_paths = (
-            (grid_dir / run_desc['grid']['coordinates'], 'coordinates.nc'),
-            (grid_dir / run_desc['grid']['bathymetry'], 'bathy_meter.nc')
-        )
+        grid_paths = ((grid_dir / coords_path, 'coordinates.nc'),
+                      (grid_dir / bathy_path, 'bathy_meter.nc'))
     run_dir_path = Path(run_dir)
     for source, link_name in grid_paths:
         if not source.exists():
