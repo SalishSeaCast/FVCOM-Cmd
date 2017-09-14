d3740fed9a59e7825a31a5f5182e0e40a25eaab0
Michael Dunphy <mdunphy@users.noreply.github.com>
2017-09-14 10:27:57 -0700

    Make prepare and run work for FVCOM

--- fvcom_cmd/run.py
+++ fvcom_cmd/run.py
@@ -18,6 +18,7 @@ Prepare for, execute, and gather the results of a run of the FVCOM model.
 """
 from __future__ import division
 
+import datetime
 import logging
 import math
 import os
@@ -32,7 +33,7 @@ import cliff.command
 
 from fvcom_cmd import api, lib
 from fvcom_cmd.fspath import fspath
-from fvcom_cmd.prepare import get_run_desc_value, get_n_processors
+#from fvcom_cmd.prepare import get_run_desc_value
 
 logger = logging.getLogger(__name__)
 
@@ -69,14 +70,6 @@ class Run(cliff.command.Command):
             help='''
             Maximum number of concurrent sub-processes to
             use for netCDF deflating. Defaults to 4.'''
-        )
-        parser.add_argument(
-            '--fvcom3.4',
-            dest='fvcom34',
-            action='store_true',
-            help='''
-            Do a FVCOM-3.4 run;
-            the default is to do a FVCOM-3.6 run'''
         )
         parser.add_argument(
             '--nocheck-initial-conditions',
@@ -127,7 +120,7 @@ class Run(cliff.command.Command):
         """
         qsub_msg = run(
             parsed_args.desc_file, parsed_args.results_dir,
-            parsed_args.max_deflate_jobs, parsed_args.fvcom34,
+            parsed_args.max_deflate_jobs,
             parsed_args.nocheck_init, parsed_args.no_submit,
             parsed_args.waitjob, parsed_args.quiet
         )
@@ -139,7 +132,6 @@ def run(
     desc_file,
     results_dir,
     max_deflate_jobs=4,
-    fvcom34=False,
     nocheck_init=False,
     no_submit=False,
     waitjob=0,
@@ -164,9 +156,6 @@ def run(
     :param int max_deflate_jobs: Maximum number of concurrent sub-processes to
                                  use for netCDF deflating.
 
-    :param boolean fvcom34: Prepare a FVCOM-3.4 run;
-                           the default is to prepare a FVCOM-3.6 run
-
     :param boolean nocheck_init: Suppress initial condition link check
                                  the default is to check
 
@@ -184,30 +173,21 @@ def run(
               run script.
     :rtype: str
     """
-    run_dir = api.prepare(desc_file, fvcom34, nocheck_init)
+    run_dir = api.prepare(desc_file, nocheck_init)
     if not quiet:
         logger.info('Created run directory {}'.format(run_dir))
-    run_desc = lib.load_run_desc(desc_file)
-    fvcom_processors = get_n_processors(run_desc,run_dir)
-    separate_xios_server = get_run_desc_value(
-        run_desc, ('output', 'separate XIOS server')
-    )
-    if not fvcom34 and separate_xios_server:
-        xios_processors = get_run_desc_value(
-            run_desc, ('output', 'XIOS servers')
-        )
-    else:
-        xios_processors = 0
+
+    # Make results directory
     results_dir = Path(results_dir)
     results_dir.mkdir()
-    batch_script = _build_batch_script(
-        run_desc,
-        fspath(desc_file), fvcom_processors, xios_processors, max_deflate_jobs,
-        results_dir, run_dir
-    )
+
+    # Build the batch script
+    batch_script = _build_batch_script(fspath(desc_file), results_dir, run_dir)
     batch_file = run_dir / 'FVCOM.sh'
     with batch_file.open('wt') as f:
         f.write(batch_script)
+
+    # Submission
     if no_submit:
         return
     starting_dir = Path.cwd()
@@ -221,172 +201,135 @@ def run(
     return qsub_msg
 
 
-def _build_batch_script(
-    run_desc, desc_file, fvcom_processors, xios_processors, max_deflate_jobs,
-    results_dir, run_dir
-):
+def _build_batch_script(desc_file, results_dir, run_dir):
     """Build the Bash script that will execute the run.
-
-    :param dict run_desc: Run description dictionary.
-
-    :param str desc_file: File path/name of the YAML run description file.
-
-    :param int fvcom_processors: Number of processors that FVCOM will be executed
-                                on.
-
-    :param int xios_processors: Number of processors that XIOS will be executed
-                                on.
-
-    :param int max_deflate_jobs: Maximum number of concurrent sub-processes to
-                                 use for netCDF deflating.
-
-    :param results_dir: Path of the directory in which to store the run
-                        results;
-                        it will be created if it does not exist.
-    :type results_dir: :py:class:`pathlib.Path`
-
-    :param run_dir: Path of the temporary run directory.
-    :type run_dir: :py:class:`pathlib.Path`
-
-    :returns: Bash script to execute the run.
-    :rtype: str
     """
-    script = u'#!/bin/bash\n'
-    email = get_run_desc_value(run_desc, ('email',))
-    script = u'\n'.join((
-        script, u'{sge_common}\n'.format(
-            sge_common=api.sge_common(
-                run_desc, email, results_dir
-            )
+    # Common header
+    script = (
+        u'#!/bin/bash\n\n'
+        u'#$ -S /bin/bash\n'
         )
-    ))
+
+    run_desc = lib.load_run_desc(desc_file)
+
+    if 'run_id' in run_desc:
+        script += (
+        u'#$ -N {run_id}\n'
+        ).format(run_id=run_desc['run_id'])
+
+    if 'email' in run_desc:
+        script += (
+        u'# email when the job [b]egins and [e]nds, or is [a]borted\n'
+        u'#$ -m bea\n'
+        u'#$ -M {email}\n'
+        ).format(email=run_desc['email'])
+
+    if 'walltime' in run_desc:
+        try:
+            td = datetime.timedelta(seconds=run_desc['walltime'])
+        except TypeError:
+            t = datetime.datetime.strptime(
+                run_desc['walltime'], '%H:%M:%S'
+            ).time()
+            td = datetime.timedelta(
+                hours=t.hour, minutes=t.minute, seconds=t.second
+            )
+        walltime = lib.td2hms(td)
+        script += (
+            u'# job runtime\n'
+            u'#$ -l h_rt={walltime}\n'
+        ).format(walltime=walltime)
+
+    # stdout/stderr
+    script += (
+        u'# stdout and stderr file paths/names\n'
+        u'#$ -o {results_dir}/stdout\n'
+        u'#$ -e {results_dir}/stderr\n'
+    ).format(results_dir=results_dir)
+
+
+    # SGE
     if 'SGE resources' in run_desc:
-        script = u''.join((
-            script[:-1],
+        script += (
             '# resource(s) requested in run description YAML file\n'
-        ))
-        script = u''.join((
-            script, u'{sge_resources}\n'.format(
-                sge_resources=_sge_resources(
-                    run_desc['SGE resources'],
-                    fvcom_processors + xios_processors
-                )
-            )
-        ))
-    script = u'\n'.join((
-        script, u'{defns}\n'.format(
-            defns=_definitions(run_desc, desc_file, run_dir, results_dir),
         )
-    ))
-    if 'modules to load' in run_desc:
-        script = u'\n'.join((
-            script, u'{modules}\n'.format(
-                modules=_modules(run_desc['modules to load'],loadcmd=u'. ssmuse-sh -d'),
+        resources = run_desc['SGE resources']
+        for resource in resources:
+            if 'res_cpus' in resource:
+                _, ppn = resource.rsplit('=', 1)
+                nproc = run_desc['nproc']
+                nnodes = math.ceil(nproc / int(ppn))
+                script += (
+                    u'#$ -pe dev {nnodes}\n'.format(nnodes=int(nnodes))
+                    )
+            script += (
+                u'#$ -l {resource}\n'.format(resource=resource)
             )
-        ))
-    script = u'\n'.join((
-        script, u'{execute}\n'
-        u'{fix_permissions}\n'
-        u'{cleanup}'.format(
-            execute=_execute(
-                fvcom_processors, xios_processors, max_deflate_jobs
-            ),
-            fix_permissions=_fix_permissions(),
-            cleanup=_cleanup(),
-        )
-    ))
-    return script
 
-def _sge_resources(resources, n_processors):
-    sge_directives = u''
-    for resource in resources:
-        if 'res_cpus' in resource:
-            _, ppn = resource.rsplit('=', 1)
-            nodes = math.ceil(n_processors / int(ppn))
-            sge_directives = u''.join((
-                sge_directives, u'#$ -pe dev {nodes}\n'.format(nodes=int(nodes))))
-        sge_directives = u''.join((
-            sge_directives, u'#$ -l {resource}\n'.format(resource=resource)
-        ))
-    return sge_directives
-
-
-def _definitions(run_desc, run_desc_file, run_dir, results_dir):
-    defns = (
+    script += (
+        u'\n'
         u'RUN_ID="{run_id}"\n'
         u'RUN_DESC="{run_desc_file}"\n'
         u'WORK_DIR="{run_dir}"\n'
         u'RESULTS_DIR="{results_dir}"\n'
-        u'COMBINE="{fvcom_cmd} combine"\n'
         u'DEFLATE="{fvcom_cmd} deflate"\n'
-        u'GATHER="{fvcom_cmd} gather"\n'
+        u'GATHER="{fvcom_cmd} gather"\n\n'
     ).format(
-        run_id=get_run_desc_value(run_desc, ('run_id',)),
-        run_desc_file=run_desc_file,
-        run_dir=run_dir,
-        results_dir=results_dir,
-        fvcom_cmd=Path('${HOME}/.local/bin/fvcom'),
+    run_id=run_desc['run_id'],
+    run_desc_file=desc_file,
+    run_dir=run_dir,
+    results_dir=results_dir,
+    fvcom_cmd=Path('${HOME}/.local/bin/fvc')
     )
-    return defns
 
 
-def _modules(modules_to_load, loadcmd=u'module load'):
-    modules = u''
-    for module in modules_to_load:
-        modules = u''.join(
-            (modules, u'{loadcmd} {module}\n'.format(loadcmd=loadcmd,module=module))
-        )
-    return modules
+    if 'modules to load' in run_desc:
+        loadcmd = '. ssmuse-sh -d'
+        modules = run_desc['modules to load']
+        for module in modules:
+            script += (
+            u'{loadcmd} {module}\n'.format(loadcmd=loadcmd,module=module)
+            )
 
-
-def _execute(fvcom_processors, xios_processors, max_deflate_jobs):
-    mpirun = u'time mpirun -np {procs} --report-bindings --bind-to-core --bycore -loadbalance ./fvcom.exe'.format(procs=fvcom_processors)
-    if xios_processors:
-        mpirun = u' '.join(
-            (mpirun, ':', '-np', str(xios_processors), './xios_server.exe')
-        )
-    script = (
+    # execution part
+    script += (
+        u'\n'
         u'cd ${WORK_DIR}\n'
-        u'echo "working dir: $(pwd)"\n'
+        u'echo "Working dir: $(pwd)"\n'
         u'\n'
         u'echo "Starting run at $(date)"\n'
         u'mkdir -p ${RESULTS_DIR}\n'
+        u'\n'
     )
-    script += u'{mpirun}\n'.format(mpirun=mpirun)
+
+    # mpirun
+    script += (
+        u'time mpirun -np {nproc} --report-bindings --bind-to-core --bycore -loadbalance ./fvcom --casename={casename} --logfile=fvcom.log\n'
+#        u'time timeout -s 9 6s mpirun -np {nproc} ./fvcom --casename={casename} --logfile=fvcom.log\n'
+    ).format(nproc=nproc, casename=run_desc['casename'])
+
     script += (
         u'MPIRUN_EXIT_CODE=$?\n'
         u'echo "Ended run at $(date)"\n'
         u'\n'
-        u'echo "Results combining started at $(date)"\n'
-        u'${{COMBINE}} ${{RUN_DESC}} --debug\n'
-        u'echo "Results combining ended at $(date)"\n'
-        u'\n'
-        u'echo "Results deflation started at $(date)"\n'
-        u'${{DEFLATE}} *_grid_[TUVW]*.nc *_ptrc_T*.nc '
-        u'--jobs {max_deflate_jobs} --debug\n'
-        u'echo "Results deflation ended at $(date)"\n'
-        u'\n'
         u'echo "Results gathering started at $(date)"\n'
-        u'${{GATHER}} ${{RESULTS_DIR}} --debug\n'
+        u'${GATHER} ${RESULTS_DIR} --debug\n'
         u'echo "Results gathering ended at $(date)"\n'
-    ).format(max_deflate_jobs=max_deflate_jobs)
-    return script
+    )
 
-
-def _fix_permissions():
-    script = (
+    # Fix permissions
+    script += (
         u'chmod go+rx ${RESULTS_DIR}\n'
         u'chmod g+rw ${RESULTS_DIR}/*\n'
         u'chmod o+r ${RESULTS_DIR}/*\n'
+        u'\n'
     )
-    return script
 
-
-def _cleanup():
-    script = (
-        u'echo "Deleting run directory" >>${RESULTS_DIR}/stdout\n'
+    script += (
+        u'echo "Deleting run directory"\n'
         u'rmdir $(pwd)\n'
-        u'echo "Finished at $(date)" >>${RESULTS_DIR}/stdout\n'
+        u'echo "Finished at $(date)"'
         u'exit ${MPIRUN_EXIT_CODE}\n'
+        u'\n'
     )
     return script
